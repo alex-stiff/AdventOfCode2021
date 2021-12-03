d9b5af87cf220142ef3fefeacbdead8025ec9dd4
Alex Stiff <alexstiff91@gmail.com>
2021-12-03 15:50:38 +0000

    fix helper

--- helpers/input.py
+++ helpers/input.py
@@ -1,6 +1,6 @@
 def read_input(filename):
     with open(filename, 'r') as f:
-        return f.read().split()
+        return f.read().splitlines()
 
 def read_input_int(filename):
     return [int(x) for x in read_input(filename)]
